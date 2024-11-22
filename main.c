a9f572f53b6d3725b5a59c3c59011bfc3135ef04
Priyajan Swongamikha <aswongamikha@gmail.com>
2024-11-22 20:11:57 +0545

    added the function to find_root, need to check if it is working

--- main.c
+++ main.c
@@ -8,9 +8,9 @@ float calculate_range() {
     x = func(i);
 
     y = func(i - 1);
-    printf("the value of y is :%f at %d\n", y, i - 1);
-    printf("the value of x is :%f at %d\n", x, i);
-    printf("one cycle done \n");
+    // printf("the value of y is :%f at %d\n", y, i - 1);
+    // printf("the value of x is :%f at %d\n", x, i);
+    // printf("one cycle done \n");
     if (x > 0 & y < 0 | x < 0 & y > 0) {
       return i - 1;
     }
@@ -18,9 +18,44 @@ float calculate_range() {
   return 0;
 }
 
+float find_root(float a, float b, float c) {
+  float fx, fy, fc, temp = 0;
+  fx = func(a);
+  fy = func(b);
+  fc = func(c);
+  while (temp != c) {
+    printf("the value of a,b,c,fx,fy,fc ,%f,%f,%f,%f,%f,%f \n", a, b, c, fx, fy,
+           fc);
+    if (fc >= 0) {
+      if (fx >= 0 & fy < 0) {
+        a = c;
+        printf("a switched with c 1.0 \n");
+      } else {
+        b = c;
+        printf("b switched with c 1.1\n");
+      }
+
+    } else if (fc < 0) {
+      if (fx < 0 & fy >= 0) {
+        a = c;
+        printf("a switched with c 2.0\n");
+
+      } else {
+        b = c;
+        printf("b switched with c 2.1\n");
+      }
+    }
+    temp = c;
+  }
+  return c;
+}
+
 int main() {
-  float a, b;
+  float a, b, c, root;
   a = calculate_range();
   b = a + 1;
-  printf("here is the interval,%f ,%f", a, b);
+  printf("here is the interval,%f ,%f\n", a, b);
+  c = mid_point(a, b);
+  root = find_root(a, b, c);
+  printf("the value of root is %f \n", root);
 }
