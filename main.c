a0be3293c09c3fac9475c673a653d6b27f70bce9
Priyajan Swongamikha <aswongamikha@gmail.com>
2024-11-22 20:34:33 +0545

    added the feature to ask user to input range as per the question

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 
 float func(float a) { return 2 * a - log10f(a) - 7; }
 float mid_point(float a, float b) { return (a + b) / 2; }
-float calculate_range() {
+float calculate_range(int r1, int r2) {
   float x, y = 0, c;
-  for (int i = 2; i < 6; i++) {
+  for (int i = r1; i < r2 + 1; i++) {
     x = func(i);
 
     y = func(i - 1);
@@ -56,7 +56,13 @@ float find_root(float a, float b, float c) {
 
 int main() {
   float a, b, c, root;
-  a = calculate_range();
+  int range1, range2;
+  printf(
+      "bro set the range, be careful about the range you set for example for "
+      "log shit start the range at 2 then for everything else it's fine \n");
+  scanf("%d", &range1);
+  scanf("%d", &range2);
+  a = calculate_range(range1, range2);
   b = a + 1;
   printf("here is the interval,%f ,%f\n", a, b);
   c = mid_point(a, b);
